d1675416482eef5a463c5636276b7d70c46f3a27
Maikindustries <a01750145@itesm.mx>
2021-11-11 16:42:49 -0600

    porcentaje sucios

--- JuegoVida.py
+++ JuegoVida.py
@@ -7,7 +7,7 @@ Octubre 8, 2021
 
 # La clase `Model` se hace cargo de los atributos a nivel del modelo, maneja los agentes. 
 # Cada modelo puede contener múltiples agentes y todos ellos son instancias de la clase `Agent`.
-from mesa import Agent, Model 
+from mesa import Agent, Model, model 
 
 # Debido a que necesitamos un solo agente por celda elegimos `SingleGrid` que fuerza un solo objeto por celda.
 from mesa.space import MultiGrid
@@ -64,6 +64,11 @@ class Robot(Agent):
                 print(cellmate.pos)
                 self.model.grid._remove_agent(self.pos, cellmate) 
                 algoSucio = True
+                self.model.num_dirty -= 1 #se elimino uno unu $$$
+                #print(self.model.num_dirty)#imprime cuantos hay actualmente $$$
+            self.model.listaSucios.append(self.model.num_dirty) # esta lista nos puede servir 
+                                                          #para graficar cuantos sucios hay en cada step $$$
+            print(self.model.listaSucios) #imprime la lista para ver que funciona(se puede comentar)$$$
         if not algoSucio:
             self.move()
 
@@ -78,7 +83,7 @@ class Robot(Agent):
         self.model.grid.move_agent(self, new_position)  
 
 
-def obtain_info(model):
+def obtain_info(model):#Esta funcion es para guardar el grid en cada step pero no me jalo con datacollector
     """Sirve para obtener información y regresarla para guardarla."""
     grid = np.zeros((model.grid.width, model.grid.height))
     for cell in model.grid.coord_iter():
@@ -97,6 +102,10 @@ class GameLifeModel(Model):
         #self.schedule = RandomActivation(self) ? 
         self.running = True #Para la visualizacion usando navegador
         
+        
+        self.listaSucios = []#lista para graficar cunatos sucios habia en cada step$$$
+        self.numberOfIterations = 100#numero de veces que se va a correr el programa (steps) $$$
+        self.num_dirty = 0#numero e sucios en el ambiente$$$
         #Guardar info
         #self.datacollector = DataCollector({"info": obtain_info})
 
@@ -106,6 +115,8 @@ class GameLifeModel(Model):
                 new_Sucio = Sucio((x, y), self)
                 self.grid._place_agent((x, y), new_Sucio)
                 self.schedule.add(new_Sucio)
+                self.num_dirty += 1 #cuantos Sucios se crearon $$$
+        self.listaSucios.append(self.num_dirty)
         for i in range (N):
             a = Robot(i, self)
             self.grid.place_agent(a, (1, 1))
@@ -115,6 +126,11 @@ class GameLifeModel(Model):
     def step(self):
         #Guardar info
         # self.datacollector.collect(self)
-        self.schedule.step()
+        self.numberOfIterations -= 1#se resta para que llegue a cero$$$
+        if self.numberOfIterations > 0:#si no es cero sigue corriendo$$$
+            self.schedule.step()
+        else:#llego a cero, se para el programa$$$
+            self.stop()#no se como parar el modelo desde la clase $$$
+            print(self.num_dirty/self.num_agents)#porcentaje de casillas sucias en el ambiente $$$
 
 
